d3320d6ea7f649f0ab1049d06757f085ff77b66b
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-23 18:42:54 +0100

    Google Search Page

---
+++ Selenium-Junit-Template/src/test/groovy/com/tech/selenium/pageobjects/GoogleSearchPage.groovy
@@ -0,0 +1,29 @@
+package com.tech.selenium.pageobjects
+
+import org.openqa.selenium.Keys
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+
+class GoogleSearchPage {
+
+    WebDriver driver
+
+    @FindBy(name = "q")
+    WebElement searchBox
+
+    GoogleSearchPage(driver) {
+        this.driver = driver
+        PageFactory.initElements(driver, this)
+    }
+
+    def openURL() {
+        driver.get("https://www.google.com/")
+    }
+
+    def searchFor(searchTerm) {
+        searchBox.sendKeys(searchTerm + Keys.RETURN)
+    }
+
+}
