4c72289c7c355c4a27e2b6b3e118f0e73c5ef0ae
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-23 18:43:23 +0100

    Test Runner

---
+++ Selenium-Junit-Template/src/test/groovy/com/tech/selenium/tests/RunTest.groovy
@@ -0,0 +1,36 @@
+package com.tech.selenium.tests
+
+import com.tech.selenium.driverutil.DriverFactory
+import com.tech.selenium.pageobjects.GoogleSearchPage
+import com.tech.selenium.pageobjects.SearchResultsPage
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.openqa.selenium.WebDriver
+
+class RunTest{
+
+    WebDriver browser
+    GoogleSearchPage googleSearchPage
+    SearchResultsPage searchResultsPage
+
+    @Before
+    void setUp() {
+        browser = DriverFactory.driver
+        googleSearchPage = new GoogleSearchPage(browser)
+        searchResultsPage = new SearchResultsPage(browser)
+    }
+
+    @Test
+    void GoogleSearchPage() {
+        googleSearchPage.openURL()
+        googleSearchPage.searchFor("Selenium")
+        searchResultsPage.assertSeleniumPresent()
+    }
+
+    @After
+    void tearDown() {
+        browser.quit()
+    }
+
+}
