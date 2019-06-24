04ed276f766f31ffdf8c62aa764220d511e6e68f
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-24 21:30:27 +0100

    Search Results Page

---
+++ Selenium-Testng-Template/src/test/groovy/com/tech/selenium/pageobjects/SearchResultsPage.groovy
@@ -0,0 +1,26 @@
+package com.tech.selenium.pageobjects
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+
+/**
+ * Created by Sridhar Bandi on 24/06/19.
+ */
+class SearchResultsPage {
+
+    WebDriver driver
+
+    @FindBy(partialLinkText  = "Selenium - Web Browser Automation")
+    WebElement lnkSelenium
+
+    SearchResultsPage(driver) {
+        this.driver = driver
+        PageFactory.initElements(driver, this)
+    }
+
+    def seleniumPresent(){
+        return lnkSelenium.isDisplayed()
+    }
+}
