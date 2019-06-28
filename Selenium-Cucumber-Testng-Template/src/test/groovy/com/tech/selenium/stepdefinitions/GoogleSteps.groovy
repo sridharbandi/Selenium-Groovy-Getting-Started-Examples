356f7b9c6828a6d40e7a850bc2819a6ade22df8a
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-28 16:55:07 +0100

    Step Definitions File

---
+++ Selenium-Cucumber-Testng-Template/src/test/groovy/com/tech/selenium/stepdefinitions/GoogleSteps.groovy
@@ -0,0 +1,41 @@
+package com.tech.selenium.stepdefinitions
+
+import com.tech.selenium.driverutil.DriverFactory
+import com.tech.selenium.pageobjects.GoogleSearchPage
+import com.tech.selenium.pageobjects.SearchResultsPage
+import org.openqa.selenium.WebDriver
+import org.testng.Assert
+
+import static cucumber.api.groovy.Hooks.After
+import static cucumber.api.groovy.Hooks.Before
+import static cucumber.api.groovy.EN.*
+
+/**
+ * Created by Sridhar Bandi on 28/06/19.
+ */
+
+WebDriver browser
+GoogleSearchPage googleSearchPage
+SearchResultsPage searchResultsPage
+
+Before() {
+    browser = DriverFactory.driver
+    googleSearchPage = new GoogleSearchPage(browser)
+    searchResultsPage = new SearchResultsPage(browser)
+}
+
+Given(~"User is on Google search page") { ->
+    googleSearchPage.openURL()
+}
+
+When(~"User searches for Selenium") { ->
+    googleSearchPage.searchFor("Selenium")
+}
+
+Then(~"User can see Selenium results") { ->
+    Assert.assertTrue(searchResultsPage.seleniumPresent())
+};
+
+After() {
+    browser.quit()
+}
