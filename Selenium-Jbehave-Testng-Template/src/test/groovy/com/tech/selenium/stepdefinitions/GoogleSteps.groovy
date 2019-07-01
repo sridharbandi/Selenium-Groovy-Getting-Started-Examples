76f03266ba22b6c2d0fc13c6c456b6cf545cf9ec
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-01 21:53:33 +0100

    Step Definitions

---
+++ Selenium-Jbehave-Testng-Template/src/test/groovy/com/tech/selenium/stepdefinitions/GoogleSteps.groovy
@@ -0,0 +1,42 @@
+package com.tech.selenium.stepdefinitions
+
+import com.tech.selenium.driverutil.DriverFactory
+import com.tech.selenium.pageobjects.GoogleSearchPage
+import com.tech.selenium.pageobjects.SearchResultsPage
+import org.jbehave.core.annotations.*
+import org.junit.Assert
+import org.openqa.selenium.WebDriver
+
+class GoogleSteps {
+
+    WebDriver browser
+    GoogleSearchPage googleSearchPage
+    SearchResultsPage searchResultsPage
+
+    @BeforeScenario
+    def setUp() {
+        browser = DriverFactory.driver
+        googleSearchPage = new GoogleSearchPage(browser)
+        searchResultsPage = new SearchResultsPage(browser)
+    }
+
+    @Given("User is on Google search page")
+    def userOnGooglePage() {
+        googleSearchPage.openURL()
+    }
+
+    @When("User searches for Selenium")
+    def userSearches() {
+        googleSearchPage.searchFor("Selenium")
+    }
+
+    @Then("User can see Selenium results")
+    def userSeesResults() {
+        Assert.assertTrue(searchResultsPage.seleniumPresent())
+    }
+
+    @AfterScenario
+    def tearDown() {
+        browser.quit()
+    }
+}
