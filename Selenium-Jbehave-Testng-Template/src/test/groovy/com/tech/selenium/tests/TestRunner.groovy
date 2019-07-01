a2ccd969a3569c1dbfd4c139298d752fbd4d51a1
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-01 21:53:45 +0100

    Test Runner

---
+++ Selenium-Jbehave-Testng-Template/src/test/groovy/com/tech/selenium/tests/TestRunner.groovy
@@ -0,0 +1,75 @@
+package com.tech.selenium.tests
+
+import com.tech.selenium.stepdefinitions.GoogleSteps
+import org.jbehave.core.configuration.Configuration
+import org.jbehave.core.configuration.MostUsefulConfiguration
+import org.jbehave.core.embedder.Embedder
+import org.jbehave.core.failures.FailureStrategy
+import org.jbehave.core.io.CodeLocations
+import org.jbehave.core.io.LoadFromClasspath
+import org.jbehave.core.junit.JUnitStories
+import org.jbehave.core.reporters.Format
+import org.jbehave.core.reporters.StoryReporterBuilder
+import org.jbehave.core.steps.InjectableStepsFactory
+import org.jbehave.core.steps.InstanceStepsFactory
+import org.testng.annotations.Test
+
+class TestRunner extends JUnitStories {
+
+    @Override
+     Embedder configuredEmbedder() {
+        final Embedder embedder = new Embedder();
+        embedder.embedderControls()
+                .useThreads(1)
+                .doGenerateViewAfterStories(true)
+                .doIgnoreFailureInStories(false)
+                .doIgnoreFailureInView(false)
+                .doVerboseFailures(true);
+
+        final Configuration configuration = configuration();
+
+        embedder.useConfiguration(configuration);
+        embedder.useStepsFactory(stepsFactory());
+
+        return embedder;
+    }
+
+    @Override
+     Configuration configuration() {
+        return new MostUsefulConfiguration()
+                .useStoryLoader(
+                new LoadFromClasspath(this.getClass())
+        )
+                .useFailureStrategy(new FailureStrategy() {
+
+            @Override
+            public void handleFailure(Throwable throwable) throws Throwable {
+                throw throwable;
+            }
+        })
+                .useStoryReporterBuilder(
+                new StoryReporterBuilder().withCodeLocation(
+                        CodeLocations.codeLocationFromClass(this.getClass())
+                )
+                        .withFormats(
+                        Format.TXT,
+                        Format.CONSOLE
+                )
+                        .withFailureTrace(true)
+        );
+    }
+
+    @Override
+     InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new GoogleSteps());
+    }
+
+    @Test
+    void run() throws Throwable {
+        super.run();
+    }
+
+    protected List<String> storyPaths() {
+        return Arrays.asList("stories/Google.story");
+    }
+}
