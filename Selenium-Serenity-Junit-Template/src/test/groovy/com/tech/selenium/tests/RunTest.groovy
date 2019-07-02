bb322392bfb3e8e8edcf1757ad253e54aa7264ce
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-02 20:12:24 +0100

    Test Runner

---
+++ Selenium-Serenity-Junit-Template/src/test/groovy/com/tech/selenium/tests/RunTest.groovy
@@ -0,0 +1,27 @@
+package com.tech.selenium.tests
+
+import com.tech.selenium.steps.SearchSteps
+import net.serenitybdd.junit.runners.SerenityRunner
+import net.thucydides.core.annotations.Managed
+import net.thucydides.core.annotations.Steps
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.openqa.selenium.WebDriver
+
+@RunWith(SerenityRunner.class)
+class RunTest {
+
+    @Managed
+    WebDriver driver;
+
+    @Steps
+    SearchSteps searchSteps;
+
+    @Test
+     void testExample(){
+        searchSteps.openURL();
+        searchSteps.searchFor("Selenium");
+        Assert.assertTrue(searchSteps.seleniumLinkPresent());
+    }
+}
