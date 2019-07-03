b50d473cc846dd0f92999662f0bf7c1a9d5f8bb5
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-03 20:59:28 +0100

    Search Steps

---
+++ Selenium-Serenity-Cucumber-Template/src/test/groovy/com/tech/selenium/steps/SearchSteps.groovy
@@ -0,0 +1,30 @@
+package com.tech.selenium.steps
+
+import com.tech.selenium.pageobjects.GoogleSearchPage
+import com.tech.selenium.pageobjects.SearchResultsPage
+import net.thucydides.core.annotations.Step
+import net.thucydides.core.steps.ScenarioSteps
+
+/**
+ * Created by Sridhar Bandi on 03/07/19.
+ */
+class SearchSteps extends ScenarioSteps {
+
+    GoogleSearchPage googleSearchPage;
+    SearchResultsPage searchResultsPage;
+
+    @Step
+    void openURL(){
+        googleSearchPage.open()
+    }
+
+    @Step
+    void searchFor(String searchterm){
+        googleSearchPage.searchFor(searchterm)
+    }
+
+    @Step
+    def seleniumLinkPresent(){
+        return searchResultsPage.seleniumPresent()
+    }
+}
