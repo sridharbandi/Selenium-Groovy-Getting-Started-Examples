3b5e45d56b9965afb4cb03d2bb81b7baac023348
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-04 20:50:46 +0100

    Google Search Page

---
+++ Selenium-Selenide-Junit-Template/src/test/groovy/com/tech/selenium/pageobjects/GoogleSearchPage.groovy
@@ -0,0 +1,24 @@
+package com.tech.selenium.pageobjects
+
+import com.codeborne.selenide.SelenideElement
+
+import static com.codeborne.selenide.Selectors.byName
+import static com.codeborne.selenide.Selenide.$
+import static com.codeborne.selenide.Selenide.open
+
+/**
+ * Created by Sridhar Bandi on 04/07/19.
+ */
+class GoogleSearchPage {
+
+    private SelenideElement searchBox = $(byName("q"));
+
+    void openURL(){
+        open("https://www.google.com/");
+    }
+
+    void searchFor(String searchTerm){
+        searchBox.val(searchTerm).pressEnter();
+    }
+
+}
