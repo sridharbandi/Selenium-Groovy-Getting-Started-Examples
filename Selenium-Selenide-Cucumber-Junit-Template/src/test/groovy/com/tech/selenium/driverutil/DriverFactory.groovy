3f00a031434aeb6e002d815ef4759119b25510ef
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-08 19:08:37 +0100

    Driver Factory Class

---
+++ Selenium-Selenide-Cucumber-Junit-Template/src/test/groovy/com/tech/selenium/driverutil/DriverFactory.groovy
@@ -0,0 +1,40 @@
+package com.tech.selenium.driverutil
+
+import com.codeborne.selenide.Configuration
+
+/**
+ * Created by Sridhar Bandi on 08/07/19.
+ */
+class DriverFactory {
+
+    def static setBrowser() {
+
+
+        def browserName = System.getProperty("browser", DriverType.CHROME.toString()).toUpperCase()
+        def driverType = DriverType.valueOf(browserName)
+
+        switch (driverType) {
+            case DriverType.CHROME:
+                System.setProperty("selenide.browser", "chrome");
+                break
+            case DriverType.FIREFOX:
+                System.setProperty("selenide.browser", "firefox");
+                break;
+            case DriverType.EDGE:
+                System.setProperty("selenide.browser", "edge");
+                break;
+            case DriverType.IE:
+                System.setProperty("selenide.browser", "ie");
+                break;
+            case DriverType.OPERA:
+                System.setProperty("selenide.browser", "opera");
+                break;
+            default:
+                System.setProperty("selenide.browser", "chrome");
+                break;
+        }
+        Configuration.startMaximized = true;
+        Configuration.timeout = 60000;
+    }
+
+}
