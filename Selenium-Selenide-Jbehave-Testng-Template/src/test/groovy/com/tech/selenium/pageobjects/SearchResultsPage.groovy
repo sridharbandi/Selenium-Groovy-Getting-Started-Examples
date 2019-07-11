525bb36ad1ea6474c279fcb6151832d82ebf8f4e
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-11 21:39:06 +0100

    Search Results Page

---
+++ Selenium-Selenide-Jbehave-Testng-Template/src/test/groovy/com/tech/selenium/pageobjects/SearchResultsPage.groovy
@@ -0,0 +1,19 @@
+package com.tech.selenium.pageobjects
+
+import com.codeborne.selenide.Condition
+import com.codeborne.selenide.SelenideElement
+
+import static com.codeborne.selenide.Selectors.byPartialLinkText
+import static com.codeborne.selenide.Selenide.$
+
+/**
+ * Created by Sridhar Bandi on 11/07/19.
+ */
+class SearchResultsPage {
+
+    private SelenideElement lnkSelenium = $(byPartialLinkText("Selenium - Web Browser Automation"));
+
+    SelenideElement getLnkSelenium(){
+        return lnkSelenium.waitUntil(Condition.appear, 60000);
+    }
+}
