56aaddb496389e9f4ddcd8ba2eb1dfa61d0ec40e
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-17 21:33:42 +0100

    Search Results Page

---
+++ Selenium-Geb-Spec-Template/src/test/groovy/com/tech/selenium/pageobjects/SearchResultsPage.groovy
@@ -0,0 +1,15 @@
+package com.tech.selenium.pageobjects
+
+import geb.Page
+
+/**
+ * Created by Sridhar Bandi on 17/07/19.
+ */
+class SearchResultsPage extends Page{
+    static at = { $("a#logo") }
+
+    static content = {
+        linkSelenium {$("a", text: contains("Selenium - Web Browser Automation"))}
+    }
+
+}
