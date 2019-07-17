a694d240a01c24e845abea6fc74b1768ead486ee
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-17 21:33:23 +0100

    Google Search Page

---
+++ Selenium-Geb-Spec-Template/src/test/groovy/com/tech/selenium/pageobjects/GoogleSearchPage.groovy
@@ -0,0 +1,22 @@
+package com.tech.selenium.pageobjects
+
+import geb.Page
+import org.openqa.selenium.By
+import org.openqa.selenium.Keys
+
+/**
+ * Created by Sridhar Bandi on 17/07/19.
+ */
+class GoogleSearchPage extends Page {
+
+    static at = { title == "Google" }
+
+    static content = {
+        searchBox {$(By.name("q"))}
+    }
+
+    def searchFor(searchText){
+        searchBox << searchText
+        searchBox << Keys.RETURN
+    }
+}
