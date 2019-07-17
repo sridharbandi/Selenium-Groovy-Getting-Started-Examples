5bc14d961eb7da01b889c9df27ecc115e45e7a3d
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-17 21:05:07 +0100

    Groovy Config

---
+++ Selenium-Geb-Testng-Template/src/test/resources/GebConfig.groovy
@@ -0,0 +1,47 @@
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.opera.OperaDriver
+
+/**
+ * Created by Sridhar Bandi on 17/07/19.
+ */
+
+baseUrl = 'https://www.google.com/'
+
+WebDriverManager.chromedriver().setup()
+driver = { new ChromeDriver() }
+
+environments {
+    "chrome" {
+        WebDriverManager.chromedriver().setup()
+        driver = { new ChromeDriver() }
+    }
+    "firefox" {
+        WebDriverManager.firefoxdriver().setup()
+        driver = { new FirefoxDriver() }
+    }
+    "edge" {
+        WebDriverManager.edgedriver().setup()
+        driver = { new EdgeDriver() }
+    }
+    "ie" {
+        WebDriverManager.iedriver().setup()
+        driver = { new InternetExplorerDriver() }
+    }
+    "opera" {
+        WebDriverManager.operadriver().setup()
+        _driver = { new OperaDriver() }
+    }
+}
+
+reportsDir = "target/reports"
+
+waiting {
+    timeout = 6
+    retryInterval = 0.5
+    slow { timeout = 12 }
+    reallySlow { timeout = 24 }
+}
